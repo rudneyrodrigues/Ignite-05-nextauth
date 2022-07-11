ca0efd5f61f424f395625e1b5d96fc09eaf63062
Rudney Rodrigues <rudney.un2016@gmail.com>
2022-07-11 11:31:01 -0300

    Creating permission component

--- web/src/pages/dashboard.tsx
+++ web/src/pages/dashboard.tsx
@@ -1,4 +1,5 @@
 import { NextPage } from "next";
+import { Can } from "../components/Can";
 
 import { useAuth } from "../context/AuthContext";
 import { useCan } from "../hooks/useCan";
@@ -6,42 +7,34 @@ import { setupAPIClient } from "../services/api";
 import { withSSRAuth } from "../utils/withSSRAuth";
 
 const Dashboard: NextPage = () => {
-  const { user, isAuthenticated } = useAuth();
+  const { user } = useAuth();
 
-  const useCanSeeMetrics = useCan({
-    permissions: ["metrics.list"],
-    roles: ["editor", "administrator"],
-  });
+  // const useCanSeeMetrics = useCan({
+  //   permissions: ["metrics.list"],
+  //   roles: ["editor", "administrator"],
+  // });
 
   return (
     <div>
       <h1>{user?.email}</h1>
 
-      {useCanSeeMetrics && (
-        <>
-          <div style={{
-            marginTop: "1rem",
-          }}>
-            {user?.permissions.map((permission) => {
-              return <p key={permission}>{permission}</p>;
-            })}
-          </div>
+      <Can permissions={['metrics.list']} roles={["editor", "administrator"]}>
+        <div style={{
+          marginTop: "1rem",
+        }}>
+          {user?.permissions.map((permission) => {
+            return <p key={permission}>{permission}</p>;
+          })}
+        </div>
 
-          <div style={{
-            marginTop: "1rem",
-          }}>
-            {user?.roles.map((role) => {
-              return <p key={role}>{role}</p>;
-            })}
-          </div>
-        </>
-      )}
-
-      {isAuthenticated ? <p style={{
-        marginTop: "1rem",
-      }}>Está autenticado</p> : <p style={{
-        marginTop: "1rem",
-      }}>Não esta autenticado</p>}
+        <div style={{
+          marginTop: "1rem",
+        }}>
+          {user?.roles.map((role) => {
+            return <p key={role}>{role}</p>;
+          })}
+        </div>
+      </Can>
     </div>
   )
 }
